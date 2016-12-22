1700b223981881a9accde8a8370bda26b0377836
Liu Yang <liu_yang@worksap.co.jp>
2016-12-22 19:19:57 +0900

    Improve script to deploy demo page.

--- publish-demo.sh
+++ publish-demo.sh
@@ -2,11 +2,21 @@
 
 set -e
 
-git checkout master
-elm-make examples/DrawPreview.elm --output=/tmp/elm.js
-git checkout gh-pages
-mv /tmp/elm.js .
-git add elm.js
-git commit -m 'update demo'
-git push origin gh-pages
-git checkout master
+
+dir=`mktemp -d`
+
+git clone `pwd` $dir
+
+(cd $dir && git checkout gh-pages && git remote set-url origin git@github.com:liuyang1204/elm-notation.git && git pull)
+
+elm-make examples/DrawPreview.elm --output=$dir/elm.js
+
+cp -r fonts lib $dir
+cp examples/draw.html $dir/index.html
+
+# change url path
+sed -i 's/\/elm.js/elm.js/g' $dir/index.html
+sed -i 's/\/lib/lib/g' $dir/index.html
+sed -i 's/\/fonts/fonts/g' $dir/index.html
+
+(cd $dir && git add . && git commit -m 'update demo' && git push origin gh-pages)
