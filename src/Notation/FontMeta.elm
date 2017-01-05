0ab6a1d2f0761cafeb640e9690401096ce7d0413
Liu Yang <liu_yang@worksap.co.jp>
2017-01-05 17:45:58 +0900

    Improve template format

--- src/Notation/FontMeta.elm
+++ src/Notation/FontMeta.elm
@@ -1,34 +1,38 @@
 module Notation.FontMeta exposing (..)
 
-fontName = "Bravura"
 
-engravingDefaults = {
-  beamThickness = 0.5,
-  staffLineThickness = 0.13,
-  thickBarlineThickness = 0.5,
-  thinBarlineThickness = 0.16,
-  stemThickness = 0.12
- }
+fontName =
+    "Bravura"
 
-glyphsWithAnchors = {
- noteheadHalf = {
-   stemDownNW = (
-       0.0,
-       -0.168
-   ),
-   stemUpSE = (
-       1.18,
-       0.168
-   )
- },
- noteheadBlack = {
-   stemDownNW = (
-       0.0,
-       -0.168
-   ),
-   stemUpSE = (
-       1.18,
-       0.168
-   )
-  }
- }
+
+engravingDefaults =
+    { beamThickness = 0.5
+    , staffLineThickness = 0.13
+    , thickBarlineThickness = 0.5
+    , thinBarlineThickness = 0.16
+    , stemThickness = 0.12
+    }
+
+
+glyphsWithAnchors =
+    { noteheadHalf =
+        { stemDownNW =
+            ( 0.0
+            , -0.168
+            )
+        , stemUpSE =
+            ( 1.18
+            , 0.168
+            )
+        }
+    , noteheadBlack =
+        { stemDownNW =
+            ( 0.0
+            , -0.168
+            )
+        , stemUpSE =
+            ( 1.18
+            , 0.168
+            )
+        }
+    }
